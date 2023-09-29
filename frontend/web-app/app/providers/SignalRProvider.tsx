298f8dcb43cddccfbb26dc2022b5e8077f32c9a0
txin214 <1090062661@qq.com>
2023-09-28 20:14:34 -0500

    Update secrets and apply DNS name change

--- frontend/web-app/app/providers/SignalRProvider.tsx
+++ frontend/web-app/app/providers/SignalRProvider.tsx
@@ -21,7 +21,7 @@ export default function SignalRProvider({ children, user }: Props) {
     const setCurrentPrice = useAuctionStore(state => state.setCurrentPrice);
     const addBid = useBidStore(state => state.addBid);
     const apiUrl = process.env.NODE_ENV === 'production' ?
-    'https://api.autobidmaster.com/notifications' : process.env.NEXT_PUBLIC_NOTIFY_URL
+    'https://apiauto.tianshuxin.com/notifications' : process.env.NEXT_PUBLIC_NOTIFY_URL
 
     useEffect(() => {
         const newConnection = new HubConnectionBuilder()
